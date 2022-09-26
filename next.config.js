16e04f2f508553a60d81c35d0615716d40a05510
mohsennoori <mohsen783>
2022-09-26 14:10:51 +0330

    change config next

--- next.config.js
+++ next.config.js
@@ -3,29 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['upload.wikimedia.org', 'flagcdn.com'],
+    domains: ['upload.wikimedia.org', 'flagcdn.com']
   },
-  webpack: function (config) {
-    if (config.optimization.splitChunks) {
-      config.optimization.splitChunks.cacheGroups.shared = {
-        name: 'app-other',
-        test: /\.css$/,
-        chunks: 'all',
-        enforce: true,
+  async rewrites () {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://restcountries.com/:path*'
       }
-    }
-    config.module.rules.push({
-      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          limit: 100000,
-          name: '[name].[ext]',
-        },
-      },
-    })
-    return config
-  },
+    ]
+  }
 }
 
 module.exports = nextConfig
