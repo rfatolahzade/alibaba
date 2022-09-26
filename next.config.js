66d9eccf8d12c806567ca2e8c9ce103513c7361a
mohsennoori <mohsen783>
2022-09-26 13:47:51 +0330

    fix webpack

--- next.config.js
+++ next.config.js
@@ -5,6 +5,27 @@ const nextConfig = {
   images: {
     domains: ['upload.wikimedia.org', 'flagcdn.com'],
   },
+  webpack: function (config) {
+    if (config.optimization.splitChunks) {
+      config.optimization.splitChunks.cacheGroups.shared = {
+        name: 'app-other',
+        test: /\.css$/,
+        chunks: 'all',
+        enforce: true,
+      }
+    }
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+          name: '[name].[ext]'
+        }
+      }
+    })
+    return config
+  }
 }
 
 module.exports = nextConfig
