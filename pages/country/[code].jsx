587fd07133931803986406c86c8a26700f3fb03d
mohsennoori <mohsen783>
2023-03-26 19:27:17 +0330

    get StaticProps

--- pages/country/[code].jsx
+++ pages/country/[code].jsx
@@ -1,34 +1,27 @@
-import { getCountryService } from 'api/services/country'
-import Detail from 'components/pages/code/Detail'
-import { isClientSideNavigate } from 'helper/helper'
+import {getCountryService}    from 'api/services/country'
+import Detail                 from 'components/pages/code/Detail'
+import {isClientSideNavigate} from 'helper/helper'
 
 const Code = props => <Detail {...props} />
 
 export default Code
 
-export async function getServerSideProps(context) {
-  if (isClientSideNavigate(context))
-    return {
-      props: {
-        data_server: null,
-        query_server: context.query,
-      },
-    }
+export async function getStaticProps (context) {
   try {
     const result =
       context.query.code && (await getCountryService(context.query.code))
     return {
       props: {
         data_server: result,
-        query_server: context.query,
-      },
+        query_server: context.query
+      }
     }
   } catch (e) {
     return {
       props: {
         data_server: null,
-        query_server: context.query,
-      },
+        query_server: context.query
+      }
     }
   }
 }
