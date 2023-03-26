714099e6521d5580fb4caeecd0ef903e4d52fa5c
mohsennoori <mohsen783>
2023-03-26 19:26:47 +0330

    get StaticProps

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,29 +1,24 @@
-import Home from 'components/pages/home/Home'
-import { isClientSideNavigate } from 'helper/helper'
-import { getCountriesService } from 'api/services/country'
+import Home                   from 'components/pages/home/Home'
+import {isClientSideNavigate} from 'helper/helper'
+import {getCountriesService}  from 'api/services/country'
 
-export default function Index(props) {
+export default function Index (props) {
   return <Home {...props} />
 }
-export async function getServerSideProps(context) {
-  if (isClientSideNavigate(context))
-    return {
-      props: {
-        countries: [],
-      },
-    }
+
+export async function getStaticProps (context) {
   try {
     const result_countries = await getCountriesService()
     return {
       props: {
-        countries: result_countries || [],
-      },
+        countries: result_countries || []
+      }
     }
   } catch (e) {
     return {
       props: {
-        countries: [],
-      },
+        countries: []
+      }
     }
   }
 }
