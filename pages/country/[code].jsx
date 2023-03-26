3c2f7e83c56bb4abfc6c901a1dea41a059d2d4f6
mohsennoori <mohsen783>
2023-03-26 20:18:04 +0330

    fix

--- pages/country/[code].jsx
+++ pages/country/[code].jsx
@@ -5,22 +5,22 @@ const Code = props => <Detail {...props} />
 
 export default Code
 
-export async function getStaticProps (context) {
-  try {
-    const result =
-      context.query.code && (await getCountryService(context.query.code))
-    return {
-      props: {
-        data_server: result,
-        query_server: context.query
-      }
-    }
-  } catch (e) {
-    return {
-      props: {
-        data_server: null,
-        query_server: context.query
-      }
-    }
-  }
-}
+// export async function getStaticProps (context) {
+//   try {
+//     const result =
+//       context.query.code && (await getCountryService(context.query.code))
+//     return {
+//       props: {
+//         data_server: result,
+//         query_server: context.query
+//       }
+//     }
+//   } catch (e) {
+//     return {
+//       props: {
+//         data_server: null,
+//         query_server: context.query
+//       }
+//     }
+//   }
+// }
